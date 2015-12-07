1d29bc81984d1d9fbc6186e46767fb0ce441460b
pc <c.patanjali@yahoo.com>
2015-12-06 17:00:59 -0800

    catching exceptions in dao code

--- spring-course-39/src/main/java/com/patanjali/offer/App.java
+++ spring-course-39/src/main/java/com/patanjali/offer/App.java
@@ -3,15 +3,21 @@ package com.patanjali.offer;
 import java.util.List;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.dao.DataAccessException;
 
 public class App {
 
 	public static void main(String... args) {
 		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml");
 		OfferDAO offer = (OfferDAO)ctx.getBean("offerDao");
-		List<Offer> offers = offer.getOffers();
-		for(Offer off:offers) {
-			System.out.println(off);
+		try {
+			List<Offer> offers = offer.getOffers();
+			for(Offer off:offers) {
+				System.out.println(off);
+			}
+		} catch(DataAccessException e) {
+			System.out.println("dao exception "+e.getMessage());
+			System.out.println("class "+e.getClass());
 		}
 		ctx.close();
 	}
