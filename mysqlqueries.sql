f1f29f5110d446a8c5b7b2eff77f869054563f83
pc <c.patanjali@yahoo.com>
2015-12-06 19:25:47 -0800

    chage in mysqlqueries

--- mysqlqueries.sql
+++ mysqlqueries.sql
@@ -1,3 +1,9 @@
 select * from offer;
 insert into offer (name,email,text) values ('bob','bob@bob.com','i am bob');
 insert into offer (name,email,text) values ('mike','mike@mike.com','i am mike');
+insert into offer (name,email,text) values ('mary','mary@none.com','i am mary');
+insert into offer (name,email,text) values ('kate','kate@none.com','i am kate');
+insert into offer (name,email,text) values ('kelly','kelly@none.com','i am kelly');
+insert into offer (name,email,text) values ('keeley','keeley@none.com','i am keeley');
+insert into offer (name,email,text) values ('ana','ana@none.com','i am ana');
+insert into offer (name,email,text) values ('ana','ana@none.com','i am ana');
